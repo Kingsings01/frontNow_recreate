48541a4bbcdb32aa7e8c2dccecd4c35bbfe98803
Kingsley Onuwa <kingsleyonuwa27@gmail.com>
2023-08-29 22:57:28 +0100

    frontNow.ai

--- src/App.js
+++ src/App.js
@@ -4,23 +4,27 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="App-header">
-        <img src={frontnowLogo} className="App-logo" alt="logo" />
-        <button className="Resources-btn">Resources</button>
-        <button className="Demo-btn">Book a Demo</button>
-      </div>
-      <div className="App-body">
-        <div className="Engage-div">Engage. Inspire. Convert</div>
-        <div className="Enterprise-div">
-          <h1>Enterprise-Grade AI Supercharging</h1>
-          <h1>Customer Experience</h1>
+      <section className="Section-nav">
+        <div className="App-header">
+          <img src={frontnowLogo} className="App-logo" alt="logo" />
+          <button className="Resources-btn">Resources</button>
+          <button className="Demo-btn">Book a Demo</button>
         </div>
-        <div className="">
-          <p>The most powerful SaaS solutions to revolutionize pre-sales in e-commerce. Designed to maximize conversion rates, increase average order value, and boost customer engagement</p>
-          <p>average order value, and boost customer engagement.</p>
+      </section>
+      <section className="Section-hero">
+        <div className="App-body">
+          <div className="Engage-div">Engage. Inspire. Convert</div>
+          <div className="Enterprise-div">
+            <h1>Enterprise-Grade AI Supercharging</h1>
+            <h1>Customer Experience</h1>
+          </div>
+          <div className="Long-p">
+            <p>The most powerful SaaS solutions to revolutionize pre-sales in e-commerce. Designed to maximize conversion rates, increase average order value, and boost customer engagement</p>
+          </div>
+          <button className="StartNow-btn">Start Now &#10140;</button>
         </div>
-        <button className="StartNow-btn">Start Now &#10140;</button>
-      </div>
+      </section>
+      <div className="Green-line">-</div>
     </div>
   );
 }
